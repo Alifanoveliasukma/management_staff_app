8a16f60d16b12e8bb92e10efe3f95e47db20d0b8
alifanovelia05 <69952596+Alifanoveliasukma@users.noreply.github.com>
2024-01-07 21:18:27 +0700

    cara 2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role',['lead','staf','admin'])->default('staf');
             $table->rememberToken();
             $table->timestamps();
         });
