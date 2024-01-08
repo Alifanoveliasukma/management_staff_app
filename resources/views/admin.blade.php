9beeb450761f395ac2ae92051c031677b2110abc
alifanovelia05 <69952596+Alifanoveliasukma@users.noreply.github.com>
2024-01-08 13:17:59 +0700

    fitur tampilan staf dan lead

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,34 +1,113 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Admin</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-</head>
-
-<body class="bg-secondary">
-  <div class="bg-white container-sm col-6 border my-3 rounded px-5 py-3 pb-5">
-    <h1>Halo!!</h1>
-    <div>Selamat datang di halaman admin</div>
-    <div><a href="/logout" class="btn btn-sm btn-secondary">Logout >></a></div>
-    <div class="card mt-3">
-      <ul class="list-group list-group-flush">
-        @if(Auth::user()->role == 'lead')
-        <li class="list-group-item">Menu Lead</li>
-        @endif
-        @if(Auth::user()->role == 'staf')
-        <li class="list-group-item">Menu staf</li>
-        @endif
-      </ul>
+@extends('master')
+@section('konten')
+<div class="main">
+  <!-- MAIN CONTENT -->
+  <div class="main-content">
+    <div class="container-fluid">
+      <!-- OVERVIEW -->
+      <div class="panel panel-headline">
+        <div class="panel-heading">
+          <h3 class="panel-title">Selamat Datang <strong>{{ Auth::user()->name }}</strong></h3>
+          <div>
+            @if(Auth::user()->role == 'lead')
+            <div class="panel">
+              <div class="panel-heading">
+                <h3 class="panel-title">Laporan yang sudah dibuat :</h3>
+              </div>
+              <div class="panel-body">
+                <table class="table table-hover">
+                  <thead>
+                    <tr>
+                      <th>No</th>
+                      <th>Date/Time</th>
+                      <th>Report Title</th>
+                      <th>Author</th>
+                      <th>Detail</th>
+                      <th>Files</th>
+                      <th>Aksi</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>8-1-2024</td>
+                      <td>Landing Page</td>
+                      <td>staf | alifa</td>
+                      <td>klik</td>
+                      <td>klik</td>
+                      <td>Setujui|Tolak|Proses</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            @endif
+            @if(Auth::user()->role == 'staf')
+            <div class="panel">
+              <div class="panel-heading">
+                <h3 class="panel-title">Laporan yang sudah dibuat :</h3>
+              </div>
+              <div class="panel-body">
+                <table class="table table-hover">
+                  <thead>
+                    <tr>
+                      <th>No</th>
+                      <th>Date/Time</th>
+                      <th>Report Title</th>
+                      <th>Recipient</th>
+                      <th>Detail</th>
+                      <th>Files</th>
+                      <th>Status</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>8-1-2024</td>
+                      <td>Landing Page</td>
+                      <td>Lead | rehan</td>
+                      <td>klik</td>
+                      <td>Klik</td>
+                      <td>Proses</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            @endif
+          </div>
+          {{-- @if(Auth::user()->role == 'lead')
+          <div>
+              <a href="#" class="btn btn-sm btn-secondary">Menu Lead</a>
+          </div>
+          @endif
+          @if(Auth::user()->role == 'staf')
+          <div>
+              <a href="#" class="btn btn-sm btn-secondary">Menu Staf</a>
+          </div>
+          @endif --}}
+      </div>
+      
+      </div>
+      <!-- END OVERVIEW -->
     </div>
-
   </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
-  </script>
-</body>
+  <!-- END MAIN CONTENT -->
+</div>
+  
+@endsection
 
-</html>
+{{-- <div class="bg-white container-sm col-6 border my-3 rounded px-5 py-3 pb-5">
+  <h1>Halo!!</h1>
+  <div>Selamat datang di halaman admin</div>
+  
+  <div class="card mt-3">
+    <ul class="list-group list-group-flush">
+      @if(Auth::user()->role == 'lead')
+      <li class="list-group-item">Menu Lead</li>
+      @endif
+      @if(Auth::user()->role == 'staf')
+      <li class="list-group-item">Menu staf</li>
+      @endif
+    </ul>
+  </div> --}}
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-	<title>Dashboard | Klorofil - Free Bootstrap Dashboard Template</title>
+	<title>Dashboard |</title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
@@ -28,7 +28,7 @@
 		<!-- NAVBAR -->
 		<nav class="navbar navbar-default navbar-fixed-top">
 			<div class="brand">
-				<a href="index.html"><img src="assets/img/logo-dark.png" alt="Klorofil Logo" class="img-responsive logo"></a>
+				<a href="index.html">Manajemen Sederhana</a>
 			</div>
 			<div class="container-fluid">
 				<div class="navbar-btn">
@@ -41,22 +41,9 @@
 				<div id="navbar-menu">
 					<ul class="nav navbar-nav navbar-right">
 						<li class="dropdown">
-							<ul class="dropdown-menu notifications">
-								<li><a href="#" class="notification-item"><span class="dot bg-warning"></span>System space is almost full</a></li>
-								<li><a href="#" class="notification-item"><span class="dot bg-danger"></span>You have 9 unfinished tasks</a></li>
-								<li><a href="#" class="notification-item"><span class="dot bg-success"></span>Monthly report is available</a></li>
-								<li><a href="#" class="notification-item"><span class="dot bg-warning"></span>Weekly meeting in 1 hour</a></li>
-								<li><a href="#" class="notification-item"><span class="dot bg-success"></span>Your request has been approved</a></li>
-								<li><a href="#" class="more">See all notifications</a></li>
-							</ul>
-						</li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="assets/img/user.png" class="img-circle" alt="Avatar"> <span>Samuel</span> <i class="icon-submenu lnr lnr-chevron-down"></i></a>
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span>{{ Auth::user()->name }}</span> <i class="icon-submenu lnr lnr-chevron-down"></i></a>
 							<ul class="dropdown-menu">
-								<li><a href="#"><i class="lnr lnr-user"></i> <span>My Profile</span></a></li>
-								<li><a href="#"><i class="lnr lnr-envelope"></i> <span>Message</span></a></li>
-								<li><a href="#"><i class="lnr lnr-cog"></i> <span>Settings</span></a></li>
-								<li><a href="#"><i class="lnr lnr-exit"></i> <span>Logout</span></a></li>
+								<li><a href="/logout" class="btn btn-sm btn-secondary">Logout >></a></li>
 							</ul>
 						</li>
 						<!-- <li>
@@ -72,15 +59,12 @@
 			<div class="sidebar-scroll">
 				<nav>
 					<ul class="nav">
-						<li><a href="index.html" class="active"><i class="lnr lnr-home"></i> <span>Dashboard</span></a></li>						
-						<li><a href="notifications.html" class=""><i class="lnr lnr-alarm"></i> <span>Notifications</span></a></li>
+						<li><a href="#" class="active"><i class="lnr lnr-home"></i> <span>Dashboard</span></a></li>
 						<li>
-							<a href="#subPages" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>Pages</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
+							<a href="#subPages" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>Buat Laporan</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
 							<div id="subPages" class="collapse ">
 								<ul class="nav">
 									<li><a href="page-profile.html" class="">Profile</a></li>
-									<li><a href="page-login.html" class="">Login</a></li>
-									<li><a href="page-lockscreen.html" class="">Lockscreen</a></li>
 								</ul>
 							</div>
 						</li>
@@ -90,40 +74,7 @@
 		</div>
 		<!-- END LEFT SIDEBAR -->
 		<!-- MAIN -->
-		<div class="main">
-			<!-- MAIN CONTENT -->
-			<div class="main-content">
-				<div class="container-fluid">
-					<!-- OVERVIEW -->
-					<div class="panel panel-headline">
-						<div class="panel-heading">
-							<h3 class="panel-title">Weekly Overview</h3>
-							<p class="panel-subtitle">Period: Oct 14, 2016 - Oct 21, 2016</p>
-						</div>
-						<div class="panel-body">
-							<div class="row">
-								<div class="col-md-3">
-									<div class="metric">
-										<span class="icon"><i class="fa fa-download"></i></span>
-										<p>
-											<span class="number">1,252</span>
-											<span class="title">Downloads</span>
-										</p>
-									</div>
-								</div>
-								
-							</div>
-							<div class="row">
-								
-								
-							</div>
-						</div>
-					</div>
-					<!-- END OVERVIEW -->
-				</div>
-			</div>
-			<!-- END MAIN CONTENT -->
-		</div>
+		@yield('konten')
 		<!-- END MAIN -->
 		<div class="clearfix"></div>
 		<footer>
